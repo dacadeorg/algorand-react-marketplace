4442811987e9cafed4a03692db294e2bd1f1836a
Timo Erdelt <ti.erdelt@gmail.com>
2022-04-21 14:25:36 +0200

    Use updated smart contract with image and creator instead of owner

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ export { ERC20_DECIMALS };
 
 // local (release) accounts
 // Get mnemonics from existing accounts with ./sandbox goal account export --address [account_address]
-const account1Mnemonic = "soft critic tent attract goat rally veteran parrot switch glide august sudden case accuse poverty orchard inside bottom true nasty hope list tourist ability scorpion"
+const account1Mnemonic ="office stand february neck invite day bird smart across tired average garage shine such abandon top sail midnight first deal unit enter dynamic above liar"
 const account2Mnemonic = "someone position liquid damage clog canyon cabin eternal fever episode cry gospel tennis chicken damp traffic puppy other ring clay bacon blade vague abandon tomorrow"
 export const account1 = algosdk.mnemonicToSecretKey(account1Mnemonic)
 export const account2 = algosdk.mnemonicToSecretKey(account2Mnemonic)
