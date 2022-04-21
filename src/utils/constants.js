187af60340b790bbdc90e1698221a2e2a5bda5cf
Timo Erdelt <ti.erdelt@gmail.com>
2022-04-21 11:41:53 +0200

    Read contracts from .teal using raw-loader, move accounts to constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,11 @@
+import algosdk from "algosdk";
+
 const ERC20_DECIMALS = 18;
 export { ERC20_DECIMALS };
+
+// local (release) accounts
+// Get mnemonics from existing accounts with ./sandbox goal account export --address [account_address]
+const account1Mnemonic = "soft critic tent attract goat rally veteran parrot switch glide august sudden case accuse poverty orchard inside bottom true nasty hope list tourist ability scorpion"
+const account2Mnemonic = "someone position liquid damage clog canyon cabin eternal fever episode cry gospel tennis chicken damp traffic puppy other ring clay bacon blade vague abandon tomorrow"
+export const account1 = algosdk.mnemonicToSecretKey(account1Mnemonic)
+export const account2 = algosdk.mnemonicToSecretKey(account2Mnemonic)
