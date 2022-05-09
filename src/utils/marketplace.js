f7ba16565b77398372d2966982711b6a8d664fbd
Timo Erdelt <ti.erdelt@gmail.com>
2022-05-09 19:45:02 +0200

    Move Product class to marketplace.js and delete model.js

--- src/utils/marketplace.js
+++ src/utils/marketplace.js
@@ -15,9 +15,19 @@ import {
 /* eslint import/no-webpack-loader-syntax: off */
 import approvalProgram from "!!raw-loader!../contracts/marketplace_approval.teal";
 import clearProgram from "!!raw-loader!../contracts/marketplace_clear.teal";
-import {Product} from "./models";
 import {base64ToUTF8String, utf8ToBase64String} from "./conversions";
 
+class Product {
+    constructor(name, image, description, price, sold, appId, owner) {
+        this.name = name;
+        this.image = image;
+        this.description = description;
+        this.price = price;
+        this.sold = sold;
+        this.appId = appId;
+        this.owner = owner;
+    }
+}
 
 // Compile smart contract in .teal format to program
 const compileProgram = async (programSource) => {
